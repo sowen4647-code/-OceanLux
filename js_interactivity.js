7df92494ce8b92dba5ad97c705c8081e3478ed04
Kenny <kennykutshina@cmail.carleton.ca>
2026-04-10 11:00:20 -0400

    mobile hamburger nav bar

--- js_interactivity.js
+++ js_interactivity.js
@@ -19,3 +19,29 @@ backToTopButton.addEventListener('click', () => {
 });
 
 }
+
+// JavaScript for mobile navigation toggle
+
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+const navLinks = document.querySelectorAll(".primary-navigation a");
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
+
+// close menu when link clicked
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    });
+});
